0fc577a814525a864d8d5a994e6b81376d8c2fad
Gene Kong <gyx_edu@qq.com>
2019-03-29 23:31:56 +0800

    inter: update make for remove this module

--- component.mk
+++ component.mk
@@ -2,10 +2,15 @@
 # Component Makefile
 #
 # port include has high priority than default ones.
+ifdef EASYLOGGER
 COMPONENT_ADD_INCLUDEDIRS += port
-COMPONENT_ADD_INCLUDEDIRS += Segger
 COMPONENT_ADD_INCLUDEDIRS += EasyLogger/easylogger/inc
-
 COMPONENT_SRCDIRS += port
-COMPONENT_SRCDIRS += Segger
 COMPONENT_SRCDIRS += EasyLogger/easylogger/src
+
+ifdef EASYLOGGER_JLINK_LOG
+COMPONENT_ADD_INCLUDEDIRS += Segger
+COMPONENT_SRCDIRS += Segger
+endif
+
+endif
