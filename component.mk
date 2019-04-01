bc68ef5fe85609efc0921126b3cdc044598396d1
yunxiang.gong <gyx_edu@qq.com>
2019-04-01 17:13:01 +0800

    fixed: macro define error fixed

--- component.mk
+++ component.mk
@@ -2,13 +2,13 @@
 # Component Makefile
 #
 # port include has high priority than default ones.
-ifdef EASYLOGGER
+ifdef CONFIG_EASYLOGGER
 COMPONENT_ADD_INCLUDEDIRS += port
 COMPONENT_ADD_INCLUDEDIRS += EasyLogger/easylogger/inc
 COMPONENT_SRCDIRS += port
 COMPONENT_SRCDIRS += EasyLogger/easylogger/src
 
-ifdef EASYLOGGER_JLINK_LOG
+ifdef CONFIG_EASYLOGGER_JLINK_LOG
 COMPONENT_ADD_INCLUDEDIRS += Segger
 COMPONENT_SRCDIRS += Segger
 endif
